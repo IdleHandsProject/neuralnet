19b5ceb7b70307e4af7cb581dac875a27545a555
hodginsa <sean.hodginsa@gmail.com>
2014-10-24 21:03:46 -0400

    working SVM updates

--- Project_File1.py
+++ Project_File1.py
@@ -12,8 +12,12 @@ import time
 from progressbar import ProgressBar
 
 pbar = ProgressBar()
-
-testsize = 20000
+pbar2 = ProgressBar()
+totalpoint = 100000
+testsize = totalpoint * 2/3
+checksize = totalpoint * 1/3
+print testsize
+print checksize
 
 training = Image.open("traindata.jpg").convert('L')
 th = 100
@@ -21,7 +25,9 @@ edge = training.point(lambda i: i < th and 255)
 edge = edge.filter(ImageFilter.FIND_EDGES)
 edge = np.array(edge, dtype = 'uint8')
 edge = (edge - 128)/128
+
 print edge
+
 Training = np.array(training, dtype = 'uint8')
 
 print Training
@@ -29,11 +35,10 @@ Training = (Training -128)/128
 OrigTrain = Training
 
 
-coord = np.indices(((Training.shape[0]),(Training.shape[1]))).swapaxes(0,2).swapaxes(0,1)
-extra = coord
-
-##coord = coord.reshape(1307*878,2)
+##coord = np.indices(((Training.shape[0]),(Training.shape[1]))).swapaxes(0,2).swapaxes(0,1)
+##extra = coord
 ##print coord
+
 
 Training = Training.flatten()
 
@@ -42,83 +47,80 @@ l = np.zeros(testsize, dtype=np.int)
 newtrain = np.zeros(testsize, dtype=np.int)
 x = 0
 lastx = 0
+print OrigTrain.shape[1]
+print OrigTrain.shape[0]
+
 while x < testsize:
-    w[x] = randint(11,OrigTrain.shape[1]-11)
-    l[x] = randint(11,OrigTrain.shape[0]-11)
+    w[x] = randint(11,OrigTrain.shape[0]-11)
+    l[x] = randint(11,OrigTrain.shape[1]-11)
     
     if x % 2>0:
-        if OrigTrain[l[x],w[x]] > 0:
+        if OrigTrain[w[x],l[x]] > 0:
             x = lastx
-            ##if x < 0:
-            ##    x -= 1
             print ("Not Black")
         else:
-            for box in range(-10,10):
-                if edge[l[x]+box, w[x]+box] < 1:
-                    print ("Edge found")
-                    newtrain[x] = 1
-                    x += 1
-                    break          
+            if x < (testsize / 2):
+                for box in range(-10,10):
+                    if edge[w[x]+box, l[x]+box] < 1:
+                        print ("Edge found")
+                        newtrain[x] = 0
+                        x += 1
+                        break
+            else:
+                newtrain[x] = 0
+                x += 1
+                          
     else:
-        if OrigTrain[l[x],w[x]] < 1:
+        if OrigTrain[w[x],l[x]] < 1:
             x = lastx
-            
             print ("Not White")
             
         else:
-            for box in range(-10,10):
-                if edge[l[x]+box, w[x]+box] < 1:
-                    print ("Edge found")
-                    newtrain[x] = 0
-                    x += 1
-                    break
+            if x < (testsize / 2):
+                for box in range(-10,10):
+                    if edge[w[x]+box, l[x]+box] < 1:
+                        print ("Edge found")
+                        newtrain[x] = 1
+                        x += 1
+                        break
+            else:
+                newtrain[x] = 1
+                x += 1
+                
     print x
     lastx = x
-##print w
-##print l
+w = w.reshape(testsize,1)
+l = l.reshape(testsize,1)
 
-coord = np.concatenate((l,w),axis=0)
-coord = coord.reshape(testsize,2)
+
+coord = np.concatenate((l,w),axis=1)
+print coord[:50:1]
+
+##coord = coord.reshape(testsize,2)
 ##print coord 
+print coord[:50:1]
  
 ##print newtrain[:testsize:1]
 X = coord[:testsize:1]
 y = newtrain[:testsize:1]
-##y = Training[:60000:1]
 
 
 
-clf = SVC()
+
+
 print ("Running Support Vector Machine")
 print ("...")
-clf.fit(X, y) 
+ 
 print ("Testing prediction")
-print ("Point (100,200) should be 0")
-print clf.predict([[100,200]])
-
-prediction = np.zeros( (1307,878), dtype=np.int)
-print ("Drawing Prediction Image - This may take a few minutes")
-x = 0
-for x in pbar(range(1307)):
-    for z in range(0, 878):
-	##checknum[x] = [randint(0,extra.shape[0]),randint(0,extra.shape[1])]
-	##print checknum[x]
-	prediction[x,z] = clf.predict([[x,z]]) * 255
-
-print prediction
-time.sleep(2)
-##img = np.hstack((coord,prediction))
-img = Image.fromarray(prediction, mode='L')
-img.save("predictedimage.jpg")
 
 fignum = 1
 Y = y
 # fit the model
 #for kernel in ('linear','linear','linear'):
 if fignum < 2:
-    kernel = 'linear'
+    kernel = 'rbf'
     print ("Creating Linear Graph")
-    clf = svm.SVC(kernel=kernel, gamma=2)
+    clf = svm.SVC(kernel=kernel, gamma=0, verbose=2)
     clf.fit(X, y)
 
     # plot the line, the points, and the nearest vectors to the plane
@@ -131,10 +133,10 @@ s=20,
     plt.scatter(X[:, 0], X[:, 1], c=Y, zorder=10, cmap=plt.cm.Paired)
 
     plt.axis('tight')
-    x_min = -1000
-    x_max = 1000
-    y_min = -1000
-    y_max = 1000
+    x_min = 0
+    x_max = 900
+    y_min = 1400
+    y_max = 0
 
     XX, YY = np.mgrid[x_min:x_max:200j, y_min:y_max:200j]
     Z = clf.decision_function(np.c_[XX.ravel(), YY.ravel()])
@@ -153,3 +155,34 @@ s=20,
     plt.yticks(())
     fignum = fignum + 2
 plt.show()
+
+percent = 0
+print ("Checking Error")
+for e in pbar(range(checksize)):
+    ew = randint(11,OrigTrain.shape[0]-11)
+    el = randint(11,OrigTrain.shape[1]-11)
+    check = clf.predict([[ew,el]])[0]
+    if check == OrigTrain[ew,el]:
+        percent += 1
+    
+ 
+acc = float(percent) / checksize * 100
+print ("The Percent Error is")
+print acc 
+
+testimagex = 1307
+testimagey = 878
+prediction = np.ones( (testimagex,testimagey), dtype=np.int)
+print ("Drawing Prediction Image - This may take a few minutes")
+x = 0
+for x in pbar2(range(testimagex)):
+    for z in range(0, testimagey):
+        prediction[z,x] = clf.predict([[z,x]]) * 255
+
+
+print prediction
+time.sleep(2)
+
+img = Image.fromarray(prediction, mode='L')
+img.save("predictedimage.png")
+
